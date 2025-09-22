e427ec5458a95901c49c77ecc6f3be8d127e84b3
kevon Cadogan <black.king1232@gmail.com>
2025-09-22 14:58:14 -0400

    Refactor Supabase client configuration to use environment variables for URL and anon key

--- renderer/src/lib/supabase.ts
+++ renderer/src/lib/supabase.ts
@@ -1,9 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-const supabaseUrl = "https://vlplrdjuzxmgtbflpgeg.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZscGxyZGp1enhtZ3RiZmxwZ2VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQxMzUzNzAsImV4cCI6MjAyOTcxMTM3MH0.ZoBKgxdo_nG6WecfREa_QuyNi1-XS4kkTVH7kIVRs04"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables. Please check your .env file.')
